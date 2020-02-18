c3f22a7a8e700820407099809fe7041bb12ee793
ccxiao5 <734194171@qq.com>
2020-02-18 16:44:27 +0800

    图片查看器

---
+++ experiment_3/experiment_3/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace experiment_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.driveListBox1 = new Microsoft.VisualBasic.Compatibility.VB6.DriveListBox();
+            this.dirListBox1 = new Microsoft.VisualBasic.Compatibility.VB6.DirListBox();
+            this.fileListBox1 = new Microsoft.VisualBasic.Compatibility.VB6.FileListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btPrevious = new System.Windows.Forms.Button();
+            this.btNext = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // driveListBox1
+            // 
+            this.driveListBox1.FormattingEnabled = true;
+            this.driveListBox1.Location = new System.Drawing.Point(50, 59);
+            this.driveListBox1.Name = "driveListBox1";
+            this.driveListBox1.Size = new System.Drawing.Size(187, 22);
+            this.driveListBox1.TabIndex = 0;
+            this.driveListBox1.SelectedIndexChanged += new System.EventHandler(this.driveListBox1_SelectedIndexChanged);
+            // 
+            // dirListBox1
+            // 
+            this.dirListBox1.FormattingEnabled = true;
+            this.dirListBox1.IntegralHeight = false;
+            this.dirListBox1.Location = new System.Drawing.Point(50, 83);
+            this.dirListBox1.Name = "dirListBox1";
+            this.dirListBox1.Size = new System.Drawing.Size(187, 228);
+            this.dirListBox1.TabIndex = 1;
+            this.dirListBox1.SelectedIndexChanged += new System.EventHandler(this.dirListBox1_SelectedIndexChanged);
+            this.dirListBox1.DoubleClick += new System.EventHandler(this.dirListBox1_DoubleClick);
+            // 
+            // fileListBox1
+            // 
+            this.fileListBox1.FormattingEnabled = true;
+            this.fileListBox1.Location = new System.Drawing.Point(50, 309);
+            this.fileListBox1.Name = "fileListBox1";
+            this.fileListBox1.Pattern = "*.jpg;*.bmp";
+            this.fileListBox1.Size = new System.Drawing.Size(187, 76);
+            this.fileListBox1.TabIndex = 2;
+            this.fileListBox1.SelectedIndexChanged += new System.EventHandler(this.fileListBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(265, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(642, 326);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btPrevious
+            // 
+            this.btPrevious.Location = new System.Drawing.Point(463, 400);
+            this.btPrevious.Name = "btPrevious";
+            this.btPrevious.Size = new System.Drawing.Size(80, 23);
+            this.btPrevious.TabIndex = 4;
+            this.btPrevious.Text = "上一张";
+            this.btPrevious.UseVisualStyleBackColor = true;
+            this.btPrevious.Click += new System.EventHandler(this.btPrevious_Click);
+            // 
+            // btNext
+            // 
+            this.btNext.Location = new System.Drawing.Point(567, 400);
+            this.btNext.Name = "btNext";
+            this.btNext.Size = new System.Drawing.Size(80, 23);
+            this.btNext.TabIndex = 5;
+            this.btNext.Text = "下一张";
+            this.btNext.UseVisualStyleBackColor = true;
+            this.btNext.Click += new System.EventHandler(this.btNext_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(966, 445);
+            this.Controls.Add(this.btNext);
+            this.Controls.Add(this.btPrevious);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.fileListBox1);
+            this.Controls.Add(this.dirListBox1);
+            this.Controls.Add(this.driveListBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "图片查看器";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.VisualBasic.Compatibility.VB6.DriveListBox driveListBox1;
+        private Microsoft.VisualBasic.Compatibility.VB6.DirListBox dirListBox1;
+        private Microsoft.VisualBasic.Compatibility.VB6.FileListBox fileListBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btPrevious;
+        private System.Windows.Forms.Button btNext;
+    }
+}
+
---
+++ experiment_3/experiment_3/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace experiment_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void driveListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String path= driveListBox1.Drive;//获得驱动器列表框的路径
+            dirListBox1.Path = path;//设置目录列表框路径
+        }
+
+        private void dirListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String path = dirListBox1.Path;//获得目录列表框的路径
+            fileListBox1.Path = path;//设置文件列表框路径
+        }
+        private void dirListBox1_DoubleClick(object sender, EventArgs e)
+        {
+            String path = dirListBox1.Path;//获得目录列表框的路径
+            fileListBox1.Path = path;//设置文件列表框路径
+        }
+
+        private void fileListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+            String path = fileListBox1.Path;//获取文件目录
+            String name = fileListBox1.FileName;//获取图片路径；
+            String picture = path + "\\" + name;//或者path +@"\" +name
+            pictureBox1.ImageLocation = picture;
+        }
+        private void btPrevious_Click(object sender, EventArgs e)
+        {
+            int loc = (fileListBox1.SelectedIndex - 1 + fileListBox1.Items.Count) % fileListBox1.Items.Count;//得到位置,fileListBox1.Items.Count得到文件数
+            fileListBox1.SelectedIndex = loc;
+            string path = fileListBox1.Path+"\\"+fileListBox1.get_Items(loc);
+            pictureBox1.Load(path);
+        }
+        private void btNext_Click(object sender, EventArgs e)
+        {
+            int loc = (fileListBox1.SelectedIndex + 1 + fileListBox1.Items.Count) % fileListBox1.Items.Count;//得到位置
+            fileListBox1.SelectedIndex = loc;
+            string path = fileListBox1.Path + "\\" + fileListBox1.get_Items(loc);
+            pictureBox1.Load(path);
+        }
+    }
+}
