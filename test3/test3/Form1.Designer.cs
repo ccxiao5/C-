51055f68e9c1a3167b9fca53fba2db13444f0151
ccxiao5 <734194171@qq.com>
2020-02-18 16:48:39 +0800

    文件夹模拟

---
+++ test3/test3/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace test3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.driveListBox1 = new Microsoft.VisualBasic.Compatibility.VB6.DriveListBox();
+            this.dirListBox1 = new Microsoft.VisualBasic.Compatibility.VB6.DirListBox();
+            this.fileListBox1 = new Microsoft.VisualBasic.Compatibility.VB6.FileListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.嘻嘻ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.哈哈ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // driveListBox1
+            // 
+            this.driveListBox1.FormattingEnabled = true;
+            this.driveListBox1.Location = new System.Drawing.Point(26, 45);
+            this.driveListBox1.Name = "driveListBox1";
+            this.driveListBox1.Size = new System.Drawing.Size(180, 22);
+            this.driveListBox1.TabIndex = 0;
+            this.driveListBox1.SelectedIndexChanged += new System.EventHandler(this.driveListBox1_SelectedIndexChanged);
+            // 
+            // dirListBox1
+            // 
+            this.dirListBox1.FormattingEnabled = true;
+            this.dirListBox1.IntegralHeight = false;
+            this.dirListBox1.Location = new System.Drawing.Point(26, 64);
+            this.dirListBox1.Name = "dirListBox1";
+            this.dirListBox1.Size = new System.Drawing.Size(180, 197);
+            this.dirListBox1.TabIndex = 1;
+            this.dirListBox1.SelectedIndexChanged += new System.EventHandler(this.dirListBox1_SelectedIndexChanged);
+            this.dirListBox1.DoubleClick += new System.EventHandler(this.dirListBox1_DoubleClick);
+            // 
+            // fileListBox1
+            // 
+            this.fileListBox1.FormattingEnabled = true;
+            this.fileListBox1.Location = new System.Drawing.Point(26, 260);
+            this.fileListBox1.Name = "fileListBox1";
+            this.fileListBox1.Pattern = "*.*";
+            this.fileListBox1.Size = new System.Drawing.Size(180, 88);
+            this.fileListBox1.TabIndex = 2;
+            this.fileListBox1.SelectedIndexChanged += new System.EventHandler(this.fileListBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(229, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(353, 214);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(262, 291);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "上一张";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(429, 291);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "下一张";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.嘻嘻ToolStripMenuItem,
+            this.哈哈ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 48);
+            // 
+            // 嘻嘻ToolStripMenuItem
+            // 
+            this.嘻嘻ToolStripMenuItem.Name = "嘻嘻ToolStripMenuItem";
+            this.嘻嘻ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.嘻嘻ToolStripMenuItem.Text = "嘻嘻";
+            // 
+            // 哈哈ToolStripMenuItem
+            // 
+            this.哈哈ToolStripMenuItem.Name = "哈哈ToolStripMenuItem";
+            this.哈哈ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.哈哈ToolStripMenuItem.Text = "哈哈";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 382);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.fileListBox1);
+            this.Controls.Add(this.dirListBox1);
+            this.Controls.Add(this.driveListBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.VisualBasic.Compatibility.VB6.DriveListBox driveListBox1;
+        private Microsoft.VisualBasic.Compatibility.VB6.DirListBox dirListBox1;
+        private Microsoft.VisualBasic.Compatibility.VB6.FileListBox fileListBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 嘻嘻ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 哈哈ToolStripMenuItem;
+    }
+}
+
---
+++ test3/test3/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace test3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void driveListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dirListBox1.Path = driveListBox1.Drive;
+        }
+
+        private void dirListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            fileListBox1.Path = dirListBox1.Path;
+        }
+
+        private void dirListBox1_DoubleClick(object sender, EventArgs e)
+        {
+            fileListBox1.Path = dirListBox1.Path;
+        }
+
+        private void fileListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+            pictureBox1.ImageLocation = fileListBox1.Path + @"\" + fileListBox1.FileName;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int loc = ((fileListBox1.SelectedIndex - 1 + fileListBox1.Items.Count) % fileListBox1.Items.Count);
+            fileListBox1.SelectedIndex = loc;
+            pictureBox1.ImageLocation = fileListBox1.Path + @"\" + fileListBox1.FileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int loc = ((fileListBox1.SelectedIndex + 1 + fileListBox1.Items.Count) % fileListBox1.Items.Count);
+            fileListBox1.SelectedIndex = loc;
+            pictureBox1.ImageLocation = fileListBox1.Path + @"\" + fileListBox1.FileName;
+        }
+
+    }
+}
